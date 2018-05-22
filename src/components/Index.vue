07128a08c5269033dffb358583110a4e25e918b3
zacks <bluemilk1995@gmail.com>
2018-05-22 17:04:31 +0800

    first commit

---
+++ src/components/Index.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="columns" style="background-color: #f0f0f0;">
+        <Menu></Menu>
+    </div>
+</template>
+
+<script>
+import Menu from '@/components/Menu';
+  export default {
+    name: 'Index',
+    components: {
+      Menu
+  }
+}
+</script>
+
+<style scoped>
+.columns{
+    height: 100%;
+}
+</style>
---
+++ src/components/Menu.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="column is-2">
+    <div class="menu is-dark">
+      <menus label="Administration">
+        <menu-item icon="twitter">
+          <span>子目录1</span>
+          <menus slot="sub">
+            <menu-item :to="{ path: '/components/menu/submenu1', query: { userId: 321 }}" icon="qq" :is-active="true">子子目录1</menu-item>
+            <menu-item icon="google">
+              <span>子子目录2</span>
+              <menus slot="sub">
+                <menu-item icon="home">第三级1</menu-item>
+                <menu-item icon="home">第三级2</menu-item>
+                <menu-item icon="home">
+                  <span>第三级3</span>
+                  <menus slot="sub" type="float">
+                    <menu-item icon="home">第四级1</menu-item>
+                  </menus>
+                </menu-item>
+              </menus>
+            </menu-item>
+          </menus>
+        </menu-item>
+        <menu-item icon="wechat" to="/components/menu/submenu2">子目录2</menu-item>
+        <div class="divider"></div>
+        <menu-item icon="facebook" to="/components/menu/submenu3"><span>子目录3</span></menu-item>
+      </menus>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'Menu'
+  }
+</script>
+
+<style scoped>
+.column is-2{
+    background-color: #2e3e4e;
+}
+.menu{
+    height: 100%;
+}
+</style>
